6b5f1123a86933af808e18c1c95ec802e04ce339
Björn Heinrichs <bjoern@heinrichs.pro>
2022-09-23 13:40:08 +0200

    fix: Add Vue to the project

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <h1>Hello Vite!</h1>
+  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+</template>
+
+<style scoped>
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,5 @@
+import { createApp } from 'vue'
 import './style.css'
+import App from './App.vue'
 
-const app = document.querySelector<HTMLDivElement>('#app')!
-
-app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+createApp(App).mount('#app')
