4b41b9859ba9760ec85fd9b3b1d92c9bdc29603b
Björn Heinrichs <bjoern@heinrichs.pro>
2022-09-23 19:52:49 +0200

    feat: Restore previous layout mostly

--- src/dashboard/AddConsumer.vue
+++ src/dashboard/AddConsumer.vue
@@ -1,16 +1,53 @@
 <template>
-  <vue-csv-import
-      v-model="csv"
-      :fields="{zeit: {required: true, label: 'Zeit'}, kw: {required: true, label: 'kW'}}"
-  >
-    <vue-csv-submit :url="TIGER_API_URL"></vue-csv-submit>
+  <div class="add-container">
+    <div v-if="closed" class="add" @click="closed = false">Verbraucher hinzufügen</div>
+    <div v-else class="add-form">
+      <vue-csv-import
+          v-model="csv"
+          :fields="{zeit: {required: true, label: 'Zeit'}, kw: {required: true, label: 'kW'}}"
+      >
+        <vue-csv-submit :url="TIGER_API_URL" />
+        <input type="text" placeholder="Verbraucher Name" />
 
-    <vue-csv-toggle-headers></vue-csv-toggle-headers>
-    <vue-csv-errors></vue-csv-errors>
-    <vue-csv-input></vue-csv-input>
-    <vue-csv-map :auto-match="false"></vue-csv-map>
-  </vue-csv-import>
+        <vue-csv-toggle-headers></vue-csv-toggle-headers>
+        <vue-csv-errors></vue-csv-errors>
+        <vue-csv-input></vue-csv-input>
+        <vue-csv-map :auto-match="false"></vue-csv-map>
+      </vue-csv-import>
+
+      <button @click="closed = true">X</button>
+    </div>
+  </div>
 </template>
+
+<style scoped>
+.add-container {
+  width: 100%;
+  display: flex;
+  padding-top: 1rem;
+  justify-content: flex-end;
+}
+
+.add {
+  transition: all 300ms;
+  width: fit-content;
+  padding: 1rem;
+  border-radius: 5px;
+  border: 1px solid black;
+  background-color: var(--secondaryColor);
+  cursor: pointer;
+  color: white;
+  font-size: 1rem;
+}
+
+.add-form {
+  border: 1px solid black;
+  width: 100%;
+  padding: 1rem;
+  display: flex;
+  justify-content: space-around;
+}
+</style>
 
 <script setup>
 import {
@@ -26,4 +63,7 @@ import { ref } from 'vue'
 import { TIGER_API_URL } from '../env'
 
 const csv = ref(null)
+const closed = ref(true)
+const upload = async () => {
+}
 </script>
